b199c3176b99d28ab60ff961474a69086a630030
Xavier Prat <xavierprat78@gmail.com>
2024-06-27 21:18:31 +0200

    botons i boletes funcionant

--- src/app/steps.service.ts
+++ src/app/steps.service.ts
@@ -10,22 +10,22 @@ export class StepsService {
       title: 'Dedica moltes hores',
       description:
         'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
-      img: 'assets/image1.svg',
-      bgcolor: '47CAB8',
+      img: 'assets/image1.png',
+      bgcolor: 'FCE46E',
     },
     {
       title: 'Programa projectes propis:',
       description:
         'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
-      img: 'assets/image2.svg',
+      img: 'assets/image2.png',
       bgcolor: 'DEDEDE',
     },
     {
       title: 'Procura descansar:',
       description:
         "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
-      img: 'assets/image3.svg',
-      bgcolor: 'FCE46E',
+      img: 'assets/image3.png',
+      bgcolor: '47CAB8',
     },
   ];
   getSteps() {
