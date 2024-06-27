7c7eb1265c01b9c23bbf2248596e071802d7326b
Xavier Prat <xavierprat78@gmail.com>
2024-06-27 13:52:18 +0200

    maquetatnt

--- src/app/steps.service.ts
+++ src/app/steps.service.ts
@@ -10,22 +10,22 @@ export class StepsService {
       title: 'Dedica moltes hores',
       description:
         'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
-      img: 'assets/img/image1.svg',
-      bgcolor: 'ff0000',
+      img: 'assets/image1.svg',
+      bgcolor: '47CAB8',
     },
     {
       title: 'Programa projectes propis:',
       description:
         'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
-      img: 'assets/img/image2.svg',
-      bgcolor: 'ffffff',
+      img: 'assets/image2.svg',
+      bgcolor: 'DEDEDE',
     },
     {
       title: 'Procura descansar:',
       description:
         "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
-      img: 'assets/img/image3.svg',
-      bgcolor: 'ffffff',
+      img: 'assets/image3.svg',
+      bgcolor: 'FCE46E',
     },
   ];
 
